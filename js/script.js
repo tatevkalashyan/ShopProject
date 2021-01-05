671ba79c29742c4713e84b1fcf59c27054434e53
tatevkalashyan <tatev.kalashyan91@gmail.com>
2021-01-05 17:08:46 +0400

    added product pages

--- js/script.js
+++ js/script.js
@@ -34,4 +34,38 @@ $(document).ready(function(){
         $searchTrigger.addClass('active');
     });
 
+
+    var minVal = 1, maxVal = 20; // Set Max and Min values
+// Increase product quantity on cart page
+    $(".increaseQty").on('click', function(){
+        var $parentElm = $(this).parents(".qtySelector");
+        $(this).addClass("clicked");
+        setTimeout(function(){
+            $(".clicked").removeClass("clicked");
+        },100);
+        var value = $parentElm.find(".qtyValue").val();
+        if (value < maxVal) {
+            value++;
+        }
+        $parentElm.find(".qtyValue").val(value);
+    });
+// Decrease product quantity on cart page
+    $(".decreaseQty").on('click', function(){
+        var $parentElm = $(this).parents(".qtySelector");
+        $(this).addClass("clicked");
+        setTimeout(function(){
+            $(".clicked").removeClass("clicked");
+        },100);
+        var value = $parentElm.find(".qtyValue").val();
+        if (value > 1) {
+            value--;
+        }
+        $parentElm.find(".qtyValue").val(value);
+    });
+
+
+    $(".product-size ul li, .product-color ul li").on('click', function() {
+        $(this).toggleClass("active")
+    });
+
 });
